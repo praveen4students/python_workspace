b4e35fb6de0b16050ad67e075bc0613a8e454ac5
Praveen Dyamappa <praveen.dyamappa@gmail.com>
2017-12-23 10:45:07 +0530

    Python Files

---
+++ MyPythonProject/com/dyasha/mypython/common/__init__.py
@@ -0,0 +1,13 @@
+from typing import AsyncGenerator
+i=10;
+j="abc";
+k='xyz';
+l=100.200;
+print("Hello Praveen !!!");
+print(i);
+print(j);
+print(k);
+print(l);
+print(i+j+k+l)
+#import _collections_abc.AsyncGenerator;
+print(AsyncGenerator.__slots__.__dir__());
